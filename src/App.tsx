1fd08ce559940410a631a3f5ec8e00dcf131d352
karanadekar10 <karanadekar10@gmail.com>
2024-07-04 23:30:27 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Fragment } from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <Fragment key={'App'}>
+      <p className="read-the-docs">
+        Welcome Karan
+      </p>
+    </Fragment>
+  )
+}
+
+export default App;
